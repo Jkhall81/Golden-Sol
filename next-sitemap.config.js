7346773dbf1bc977894c0771651d327a00e4e52f
Jkhall81 <jason.kei.hall@gmail.com>
2025-07-04 07:57:13 -0700

    added sitemap.xml functionality to the site.  going to submit it to google to be crawled.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://golden-sol-tanning.vercel.app", // or your custom domain
+  generateRobotsTxt: true, // ✅ also creates a robots.txt file
+  sitemapSize: 7000,
+};
