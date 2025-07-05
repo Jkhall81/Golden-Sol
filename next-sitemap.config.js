a64c0982841156d08b87d69719da829287b4b770
Jkhall81 <jason.kei.hall@gmail.com>
2025-07-05 06:34:26 -0700

    got a domain for this.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://golden-sol-tanning.vercel.app", // or your custom domain
+  siteUrl: "https://golden-sol-tanning.com", // or your custom domain
   generateRobotsTxt: true, // ✅ also creates a robots.txt file
   sitemapSize: 7000,
 };
